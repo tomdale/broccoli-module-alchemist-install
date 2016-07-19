a30c948a1451d95769b14f36fb631ed0e823cbf3
Tom Dale <tom@tomdale.net>
2016-07-19 17:29:06 -0400

    Initial commit

---
+++ bin/broccoli-module-alchemist-install.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+
+var cwd = process.cwd();
+var pkgPath = path.join(cwd, 'package.json');
+var pkg = JSON.parse(fs.readFileSync(pkgPath));
+var main = pkg.main;
+
+if (!main) {
+  throw new Error("Unable to rewrite package.json main; no existing main entry.");
+}
+
+try {
+  require(path.join(cwd(), main));
+} catch(e) {
+  // Remove leading 'src' directory.
+  // src/system/server.js -> system/server.js
+  var mainPath = main.split(path.sep).slice(1).join(path.sep);
+  // system/server.js -> dist/cjs/system/server.js
+  var cjsMain = path.join('dist', 'cjs', mainPath);
+
+  try {
+    console.log('requiring ' + cjsMain);
+    require(path.join(cwd, cjsMain));
+    console.log('Switching package main to ' + cjsMain);
+
+    pkg.main = cjsMain;
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
+  } catch (f) {
+    console.log(f);
+    console.log('Unable to find functioning main');
+  }
+}
